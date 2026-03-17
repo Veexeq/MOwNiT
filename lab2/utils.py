745175cf5c4ae6175392d15790ef1af81a3ab203
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-03-17 10:12:03 +0100

    Added testing functions to utils.py

--- lab2/utils.py
+++ lab2/utils.py
@@ -27,3 +27,29 @@ def generate_chebyshev_nodes(a: float, b: float, n: int) -> NDArray[np.float64]:
     
     return scaled_nodes
     
+def runge_function(x: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    Runge's function: f(x) = 1 / (1 + 25x^2).
+    Demonstrates the Runge's phenomenon, where the interpolating function oscilates wildly
+    at the boundaries of the interval.
+    Recommended test interval: [-1, 1] 
+    """
+    return 1.0 / (1.0 + 25.0 * x**2)
+
+def sine_function(x: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    A smooth function: f(x) = sin(x).
+    Great for showcasing that the interpolation works well on smooth functions.
+    Recommended test interval: [-np.pi, np.pi] 
+    """
+    return np.sin(x)
+
+def absolute_function(x: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    A function with a "sharp" kink: f(x) = |x|
+    Interesting example, because it's not differentiable at x = 0.
+    Interpolating a "sharp" function with polynomials that are inherently
+    smooth often leads to notable issues and significant errors near 0.
+    Recommended test interval: [-1, 1] 
+    """
+    return np.abs(x)
