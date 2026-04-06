ed02e08086919fab8503dae1408c9cdab457615c
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-04-06 09:37:08 +0200

    Removed lab2 from repo, as it is legacy now.

--- lab2/main.py
+++
@@ -1,9 +0,0 @@
-import tests
-
-def main():
-    # tests.run_all_tests()
-    tests.custom_function_lagrange_handler()
-    tests.custom_function_hermite_handler()
-
-if __name__ == '__main__':
-    main()
